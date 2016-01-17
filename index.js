0c05c75fa6ab0b614f23e3bb6d89228b9085a2d0
Ubuntu <ubuntu@ip-172-31-44-181.us-west-2.compute.internal>
2016-01-16 20:22:02 -0700

    cacheing now availiable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require('fs-extra');
 var nodePath = require('path');
 var async = require('async');
+var checksum = require('checksum');
 var findRequires = require('find-requires');
 var request = require('request');
 var _ = require('underscore');
@@ -10,6 +11,7 @@ var eswalk = require('esprima-walk');
 var escodegen = require('escodegen');
 var prequire = require('parent-require');
 var nodeResolve = require('require-resolve');
+var path = require('path');
 
 module.exports = function(config){
 	config = config||{};
@@ -82,10 +84,28 @@ module.exports = function(config){
 
 	var visited = {
 		build : [],
-		require : []
+		require : [],
 	};
 
-	function trace(traceConfig, eachModule, traceComplete, isBuild){
+	function cacheInit(config){
+		if(config.cache && !visited.cache){
+			visited.configpath = config.cache;
+			fs.ensureFileSync(visited.configpath);
+			try{
+				visited.cache = fs.readJsonSync(visited.configpath);
+			}catch(e){
+				visited.cache = {};
+			}
+			visited.newcache = {};
+		}
+	}
+
+	function cacheDone(){
+		if(visited.configpath) fs.writeFileSync(visited.configpath, JSON.stringify(visited.newcache))
+	}
+
+	function trace(traceConfig, eachModule, traceComplete, isBuild, cache){
+		traceConfig.cache = cache;
 		traceConfig.paths = traceConfig.paths||{};
 		traceConfig.deps = traceConfig.deps||[];
 		traceConfig.context = traceConfig.context||[];
@@ -117,114 +137,130 @@ module.exports = function(config){
 						open(filePath, function(e, raw){
 							if(e) traceDepComplete('failed to open: '+filePath);
 							else{
-								/* transform raw by plugins */
-								async.reduce(_.keys(pluginModules), raw, function(memo, pluginPath, transformDone){
-									var transform = pluginModules[pluginPath].transform||pluginModules[pluginPath];
-									if(_.isFunction(transform)) transform(memo, filePath, transformDone);
-									else transformDone(null, memo);
-								}, function(e, raw){
-									filePath = pluginPrefix+filePath; //add plugins back to filename
-									try{ var parse = esprima.parse(raw); }catch(e){
-										traceDepComplete(filePath+': '+e);
-									}
-									if(parse){
-										var ownConfig = normalizedConfigs[filePath]||{};
-										ownConfig.deps = ownConfig.deps||[];
-										ownConfig.paths = ownConfig.paths||{};
-										var newTraceConfig = JSON.parse(JSON.stringify(traceConfig));
-										newTraceConfig.deps = ownConfig.deps; 
-										newTraceConfig.context.push(filePath);
-										/* find what plugins will be needed at runtime */
-										var neededPlugins = _.filter(_.keys(pluginModules), function(pluginPath){
-											return _.has(pluginModules[pluginPath], 'init');
-										})
-										newTraceConfig.deps = _.uniq(newTraceConfig.deps.concat(neededPlugins)); //add needed plugins as deps
-										if(ownConfig.export){
-											/* shim from export variable */
-											var exportDeps = _.map(ownConfig.deps, function(ownDep){
-												var normOwnDep = normalize(ownDep, traceConfig, isBuild);
-												var exp = normalizedConfigs[normOwnDep] && normalizedConfigs[normOwnDep].export;
-												return exp?"var "+exp+" = require('"+normOwnDep+"');":'';
-											}).join('\n');
-											parse = esprima.parse('define({factory : true}, function(){'+exportDeps+'\n'+escodegen.generate(parse)+'\n return '+ownConfig.export+';});');
-										}else if(ownConfig.amd){
-											/* compatabilize, replace define with amd */
+								/* check against cache */
+								var check = checksum(raw+JSON.stringify(traceConfig));
+								var cached = visited.cache[check];
+								if(cached) ready(cached.config, cached.js, cached.ownConfig, check);
+								else{
+									console.log('-', filePath);
+									/* transform raw by plugins */
+									async.reduce(_.keys(pluginModules), raw, function(memo, pluginPath, transformDone){
+										var transform = pluginModules[pluginPath].transform||pluginModules[pluginPath];
+										if(_.isFunction(transform)) transform(memo, filePath, transformDone);
+										else transformDone(null, memo);
+									}, function(e, raw){
+										filePath = pluginPrefix+filePath; //add plugins back to filename
+										try{ var parse = esprima.parse(raw); }catch(e){
+											traceDepComplete(filePath+': '+e);
+										}
+										if(parse){
+											var ownConfig = normalizedConfigs[filePath]||{};
+											ownConfig.deps = ownConfig.deps||[];
+											ownConfig.paths = ownConfig.paths||{};
+											var newTraceConfig = JSON.parse(JSON.stringify(traceConfig));
+											newTraceConfig.deps = ownConfig.deps; 
+											newTraceConfig.context.push(filePath);
+											/* find what plugins will be needed at runtime */
+											var neededPlugins = _.filter(_.keys(pluginModules), function(pluginPath){
+												return _.has(pluginModules[pluginPath], 'init');
+											})
+											newTraceConfig.deps = _.uniq(newTraceConfig.deps.concat(neededPlugins)); //add needed plugins as deps
+											if(ownConfig.export){
+												/* shim from export variable */
+												var exportDeps = _.map(ownConfig.deps, function(ownDep){
+													var normOwnDep = normalize(ownDep, traceConfig, isBuild);
+													var exp = normalizedConfigs[normOwnDep] && normalizedConfigs[normOwnDep].export;
+													return exp?"var "+exp+" = require('"+normOwnDep+"');":'';
+												}).join('\n');
+												parse = esprima.parse('define({factory : true}, function(){'+exportDeps+'\n'+escodegen.generate(parse)+'\n return '+ownConfig.export+';});');
+											}else if(ownConfig.amd){
+												/* compatabilize, replace define with amd */
+												eswalk(parse, function(child){
+													if(child.callee && child.callee.name == 'define') child.callee.name = 'amd';
+													if(child.name == 'define') child.name = 'amd';
+												});
+												parse = esprima.parse('define({factory : true}, function(){return (function(){amdModules = {};'+escodegen.generate(parse)+' return amdModules["'+ownConfig.amd+'"]})()});');
+											}
 											eswalk(parse, function(child){
-												if(child.callee && child.callee.name == 'define') child.callee.name = 'amd';
-												if(child.name == 'define') child.name = 'amd';
-											});
-											parse = esprima.parse('define({factory : true}, function(){return (function(){amdModules = {};'+escodegen.generate(parse)+' return amdModules["'+ownConfig.amd+'"]})()});');
-										} //ACCOUNT FOR RAW? typewatch.js
-										eswalk(parse, function(child){
-											if(child.type == 'CallExpression' && child.callee.name == 'define'){
-												/* 2 args means inline config, remove and extend into ownConfig */
-												if(child.arguments.length == 2){
-													var inlineConfig = eval('('+escodegen.generate(child.arguments.shift())+')');
-													if(_.isArray(inlineConfig) && !ownConfig.export) inlineConfig = {deps : _.uniq(ownConfig.deps.concat(inlineConfig))};										
-													extendConfigs(ownConfig, inlineConfig); //inherit to ownConfig
-													extendConfigs(newTraceConfig, ownConfig);
+												if(child.type == 'CallExpression' && child.callee.name == 'define'){
+													/* 2 args means inline config, remove and extend into ownConfig */
+													if(child.arguments.length == 2){
+														var inlineConfig = eval('('+escodegen.generate(child.arguments.shift())+')');
+														if(_.isArray(inlineConfig) && !ownConfig.export) inlineConfig = {deps : _.uniq(ownConfig.deps.concat(inlineConfig))};										
+														extendConfigs(ownConfig, inlineConfig); //inherit to ownConfig
+														extendConfigs(newTraceConfig, ownConfig);
+													}
+													/* wrap it in a return function */
+													child.arguments[0] = {
+														type: 'FunctionExpression',
+														id: null,
+														params: [],
+														defaults: [],
+														body : {
+															type : 'BlockStatement',
+															body : [{
+																type : 'ReturnStatement',
+																argument : child.arguments[0]
+															}]
+														},
+														generator: false,
+														expression: false
+													};
+												}else if(child.type == 'CallExpression' && ( //environment exclusion fns
+												  (child.callee.name == 'browser' && !isBuild) ||
+												  (child.callee.name == 'node' && isBuild)
+												)) child.arguments = [];
+												else if(
+													child.type == 'CallExpression' &&
+													child.callee.name == 'require' &&
+													child.arguments[0] &&
+													child.arguments[0].type == 'Literal'
+												){ //find all our deps
+													var dep = child.arguments[0].value
+													ownConfig.deps = _.uniq(ownConfig.deps.concat(dep));
+													child.arguments[0].value = normalize(dep, newTraceConfig, isBuild);
 												}
-												/* wrap it in a return function */
-												child.arguments[0] = {
-													type: 'FunctionExpression',
-													id: null,
-													params: [],
-													defaults: [],
-													body : {
-														type : 'BlockStatement',
-														body : [{
-															type : 'ReturnStatement',
-															argument : child.arguments[0]
-														}]
-													},
-													generator: false,
-													expression: false
-												};
-											}else if(child.type == 'CallExpression' && ( //environment exclusion fns
-											  (child.callee.name == 'browser' && !isBuild) ||
-											  (child.callee.name == 'node' && isBuild)
-											)) child.arguments = [];
-											else if(
-												child.type == 'CallExpression' &&
-												child.callee.name == 'require' &&
-												child.arguments[0] &&
-												child.arguments[0].type == 'Literal'
-											){ //find all our deps
-												var dep = child.arguments[0].value
-												ownConfig.deps = _.uniq(ownConfig.deps.concat(dep));
-												child.arguments[0].value = normalize(dep, newTraceConfig, isBuild);
-											}
-										});
-										/* extend again to include required paths */
-										extendConfigs(newTraceConfig, ownConfig);
-										ownConfig.deps = _.map(ownConfig.deps, function(ownDep){
-											return normalize(ownDep, newTraceConfig, isBuild);
-										});
-										/* clean up ownconfig */
-										if(!ownConfig.deps.length) delete ownConfig.deps;
-										if(!_.keys(ownConfig.paths).length) delete ownConfig.paths;
-										delete ownConfig.source;
-										/* now that all deps are in, add ownconfigs at front */
-										eswalk(parse, function(child){
-											if(child.type == 'CallExpression' && child.callee.name == 'define'){
-												var parsedOwnConfig = esprima.parse('('+JSON.stringify(ownConfig)+')').body[0].expression;
-												child.arguments.unshift(parsedOwnConfig);
-												child.arguments.unshift(esprima.parse("'"+filePath+"'").body[0].expression);
-											}
-										})
-										var js = escodegen.generate(parse); //final js
-										/* fetching a property from newTraceConfig makes it inheritable */
-										if(newTraceConfig.minify) js = uglify.minify(js, {fromString: true}).code;
-										trace(newTraceConfig, eachModule, function(e){
-											eachModule(filePath, js, ownConfig);
-											traceDepComplete(e);
-										}, isBuild);
-									}
-								});
+											});
+											/* extend again to include required paths */
+											extendConfigs(newTraceConfig, ownConfig);
+											ownConfig.deps = _.map(ownConfig.deps, function(ownDep){
+												return normalize(ownDep, newTraceConfig, isBuild);
+											});
+											/* clean up ownconfig */
+											if(!ownConfig.deps.length) delete ownConfig.deps;
+											if(!_.keys(ownConfig.paths).length) delete ownConfig.paths;
+											delete ownConfig.source;
+											/* now that all deps are in, add ownconfigs at front */
+											eswalk(parse, function(child){
+												if(child.type == 'CallExpression' && child.callee.name == 'define'){
+													var parsedOwnConfig = esprima.parse('('+JSON.stringify(ownConfig)+')').body[0].expression;
+													child.arguments.unshift(parsedOwnConfig);
+													child.arguments.unshift(esprima.parse("'"+filePath+"'").body[0].expression);
+												}
+											})
+											var js = escodegen.generate(parse); //final js
+											/* fetching a property from newTraceConfig makes it inheritable */
+											if(newTraceConfig.minify) js = uglify.minify(js, {fromString: true}).code;
+											ready(newTraceConfig, js, ownConfig, check);
+										}
+									});
+								}
 							}
 						})
+
+						function ready(newTraceConfig, js, ownConfig, check){
+							visited.newcache[check] = {
+								config : newTraceConfig,
+								js : js,
+								ownConfig : ownConfig
+							}
+							trace(newTraceConfig, eachModule, function(e){
+								eachModule(filePath, js, ownConfig);
+								traceDepComplete(e);
+							}, isBuild);
+						}
 					}
-				})
+				}, true)
 			}
 		}, function(e){
 			traceComplete(e, _.uniq(visited[isBuild?'build':'require']));
@@ -233,7 +269,7 @@ module.exports = function(config){
 
 	var bilt = {
 		modules : {},
-		require : function(requireConfig, callback){
+		require : function(requireConfig, callback, internal){
 			function nodeRequire(path){
 				return prequire(path);
 			}
@@ -243,8 +279,8 @@ module.exports = function(config){
 			eval(defineJs);
 			requireConfig.deps = _.difference(requireConfig.deps, _.keys(bilt.modules));
 			if(requireConfig.verbose) console.log('Require:', requireConfig.deps);
+			if(!internal) cacheInit(requireConfig);
 			trace(requireConfig, function(path, js){
-				if(requireConfig.verbose) console.log(' - ', path);
 				function browser(){
 					return null;
 				}
@@ -253,20 +289,22 @@ module.exports = function(config){
 				}
 				eval(js);
 			}, function(e, loaded){
+				if(!internal) cacheDone();
 				waitFor(ouputPaths, function(){
 					callback(e, _.mapObject(_.pick(bilt.modules, ouputPaths), function(val){
 						return val.value;
 					}));
 				})					
-			})
+			}, false)
 		},
 		build : function(buildConfig, init, callback){
 			if(buildConfig.verbose) console.log('Build:', buildConfig.deps);
 			var output = '';
+			cacheInit(buildConfig);
 			trace(buildConfig, function(path, js){
-				if(buildConfig.verbose) console.log(' - ', _.last(path.split('!')));
 				if(js.length) output += js+'\n\n'; 
 			}, function(e, loaded){
+				cacheDone();
 				/* if no problems include the minified client lib */
 				if(e) callback(e);
 				else fs.readFile(nodePath.join(__dirname, 'lib/client.js'), 'utf8', function(e, require){
